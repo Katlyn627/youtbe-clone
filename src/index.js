41b8023c6b2f412854a9030bf0ab8b0f564f12ee
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2023-06-19 21:49:22 -0600

    add basic file structure and routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-
 import React from 'react';
-// ✅ now importing from react-dom/client
 import ReactDOM from 'react-dom/client';
 
-import './index.css'
-
 import App from './App';
+import './index.css';
 
-// 👇️ IMPORTANT: make sure to specify correct ID
-// must be the ID of the div element in your index.html file
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 
 root.render(<App />);
